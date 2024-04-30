4543684d01bb0b7a8c5fde69e539b61769aeab03
jackbasso <jackbasso.r@gmail.com>
2024-04-29 21:50:40 -0600

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { client } from "@/sanity/lib/client";
+import { groq } from "next-sanity";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import Footer from "@/components/Footer";
+import Main from "@/components/Main";
+
+const query = groq`
+*[_type == "post"] {
+  ...,
+  author->,
+} | order(_createdAt desc)
+`;
+
+const page = async () => {
+  const posts = await client.fetch(query);
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <Main />
+      <Footer />
+    </div>
+  );
+};
+
+export default page;
