d16b66702346612f564e9a6c67599e39126ddf7d
jackbasso <jackbasso.r@gmail.com>
2024-05-01 21:08:35 -0600

    hero section

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import Logo from "../public/assets/bloglogo.png";
+import Image from "next/image";
+import Link from "next/link";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <nav
+      className="flex justify-between bg-[#1C252A] text-white py-3 md:py-5 px-5 md:px-10 lg:px-20 font-semibold
+      text-[12px] md:text-[14px] lg:text-[16px] items-center"
+    >
+      <Link href="/" className="flex items-center">
+        <span>
+          <Image src={Logo} alt="Logo" className="h-8 w-8" />
+        </span>
+        <span className="hidden md:inline-block"> My Travel Blog</span>
+      </Link>
+
+      <div className="flex space-x-3">
+        <p>TRAVEL BLOG</p>
+        <p>TRAVEL REVIEWS</p>
+        <p>ABOUT</p>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import BgImg from "../public/assets/bgimg.jpg";
+import Image from "next/image";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="relative w-full h-[60vh] md:h-[75vh]">
+      <div className="absolute inset-0 -z-10">
+        <Image src={BgImg} alt="bgimg" fill className="object-cover object-center brightness-50" />
+      </div>
+      <div className="flex justify-end h-[66%] items-center">
+        {/* Nothing will be here, just for proper alignment */}
+        <div className="flex flex-col mt-10 w-[50vw]">
+          <h1 className="text-white text-[30px] md:text-[50px] font-semibold">We Take Care</h1>
+          <h1 className="text-white text-[30px] md:text-[50px] font-semibold text-center">Of Your Trip</h1>
+          <div className="flex justify-center pt-5 md:pt-10">
+            <button
+              className="text-white bg-gradient-to-r from-transparent to-[#E7493F] w-34 md:w-48 text-[13px] 
+            md:text-base hover:bg-[#E7493F] font-semibold py-2 px-4 border border-white hover:border-transparent"
+            >
+              EXPLORE NOW
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
